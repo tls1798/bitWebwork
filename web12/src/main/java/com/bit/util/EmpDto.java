60e051dbb8a66f90378dcafc57649c290a30594f
jaeseung <tls1798@naver.com>
2022-05-18 22:20:26 +0900

    22.05.13 java bean, sql module,  sql test

---
+++ web12/src/main/java/com/bit/util/EmpDto.java
@@ -0,0 +1,33 @@
+package com.bit.util;
+
+public class EmpDto {
+	private int empno, sal;
+	private String ename="";
+	private boolean result;
+	
+	public boolean isResult() {
+		return result;
+	}
+	public void setResult(boolean result) {
+		this.result = result;
+	}
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public int getSal() {
+		return sal;
+	}
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	
+}
